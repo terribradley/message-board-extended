e23dab7696d493d87eb8b3d545d35279508859aa
Terri Bradley <terribradley@gmail.com>
2016-08-27 10:05:54 -0700

    icon appears for questions that have more than 4 answers

---
+++ app/helpers/popular-question.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function popularQuestion(params) {
+  var question = params[0];
+
+ if(question.get('answers').get('length') >= 6) {
+   return Ember.String.htmlSafe('<img src="../img/icon.png" alt="icon"/>');
+ }
+}
+
+export default Ember.Helper.helper(popularQuestion);
---
+++ tests/unit/helpers/popular-question-test.js
@@ -0,0 +1,10 @@
+import { popularQuestion } from 'message-board/helpers/popular-question';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | popular question');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = popularQuestion([42]);
+  assert.ok(result);
+});
